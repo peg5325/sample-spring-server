078e24d9be551eff577d43621a9b92dfec9225e7
Eongyu Park <peg5325@gmail.com>
2025-04-30 10:54:57 +0900

    Initial commit

---
+++ src/main/java/com/example/samplespringserver/SampleSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplespringserver/SampleSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
